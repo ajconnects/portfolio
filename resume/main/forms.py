dd161af693a4dc19b343a5c9f57c75844c7c2d16
Aj connect <ajiriiviero2021@gmail.com>
2024-05-10 15:25:03 +0200

    digital cv

---
+++ resume/main/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import ContactProfile
+
+
+class ContactForm(forms.ModelForm):
+
+    name = forms.CharField(max_length=200, required=True,widget=forms.TextInput(attrs={
+        'placeholder': '*Full name..',
+        #'class': 'form-control'
+    }))
+
+    email = forms.EmailField(max_length=254, required=True,widget=forms.TextInput(attrs={
+        'placeholder': '*Email..',
+        #'class': 'form-control'
+    }))
+
+    message = forms.CharField(max_length=1000, required=True,widget=forms.Textarea(attrs={
+        'placeholder': '*Message..',
+        'rows': 6,
+        #'class': 'form-control'
+    }))
+
+    class Meta:
+        model = ContactProfile
+        fields = ('name', 'email', 'message',)
